c72bdd128124d5af79e23c0e5f9529e768faad02
rexdev0211 <rexdev0211@gmail.com>
2022-09-23 19:52:15 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,261 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const { URLSearchParams } = require('url');
+const fs = require('fs');
+
+const app = express();
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+// Jade
+app.set('views', __dirname+'/views');
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'views')));
+
+const headers = {
+  'authority': 'api.mtpelerin.com',
+  'accept': 'application/json',
+  'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
+  'content-type': 'application/json',
+  'origin': 'https://buy.mtpelerin.com',
+  'referer': 'https://buy.mtpelerin.com/',
+  'sec-ch-ua': '"Chromium";v="105", "Not)A;Brand";v="8"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"Linux"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+};
+
+const phone = '+447418354083';
+const push_id = '1234';
+const deviceInfo = {
+  uniqueId: "e4292cdb1790898989242bwfew909090",
+  user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+  type: "onofframp-widget-web-v1"
+}
+
+const amount = 200 // 2 EUR
+const currency = 'EUR';
+const paymentUserAgent = 'custom-form/v2-12f06754';
+const referrer = 'https://buy.mtpelerin.com/?type=direct-link&bdc=BUSD&addr=0x668b73934d306629d8dc83f70fc69dd6ac130f0d&lang=en&tab=buy&tabs=buy&crys=BUSD&bsc=EUR';
+const key = 'pk_live_LKPpyvLMFTxFN0W0OBnvyB0T';
+
+const bankSymbolId = '013';
+
+app.get('/', async (req, res) => {
+  res.render('charge', {
+    title: "Simple Charge Card"
+  });
+});
+
+// Step 1 - Add a new device
+app.post('/addDevice', async (req, res) => {
+  try {
+    const url = 'https://api.mtpelerin.com/devices/add';
+
+    const data = {
+      phone: req.body.phone,
+      push_id,
+      deviceInfo
+    };
+
+    const response = await axios.post(url, data, { headers });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+// Step 2 - Send sms
+app.post('/sendSms', async (req, res) => {
+  try {
+    const id = req.body.deviceId;
+    const url = `https://api.mtpelerin.com/devices/${id}/sms`;
+
+    const response = await axios.post(url, {}, { headers });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+// Step 3 - Approve device and get authorization key
+app.post('/authenticate', async (req, res) => {
+  try {
+    const id = req.body.deviceId;
+    const code = req.body.code;
+    const url = `https://api.mtpelerin.com/devices/${id}/authenticate`;
+
+    const response = await axios.post(url, { code }, { headers });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+// Step 4 - Create a card id(tok id)
+app.post('/tokens', async (req, res) => {
+  try {
+    const data = req.body;
+    const url = `https://api.securionpay.com/tokens`;
+
+    const newHeaders = { ...headers, authorization: 'Basic cGtfbGl2ZV9MS1BweXZMTUZUeEZOMFcwT0JudnlCMFQ6' };
+    const response = await axios.post(url, data, { headers: newHeaders });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+// Step 5 - get 3d-secured transaction id
+app.post('/3d-secure', async (req, res) => {
+  try {
+    const card = req.body.card;
+    const url = `https://api.securionpay.com/3d-secure`;
+
+    const newHeaders = {
+      'authority': 'api.securionpay.com',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
+      'content-type': 'application/x-www-form-urlencoded',
+      'origin': 'https://api.securionpay.com',
+      'referer': 'https://api.securionpay.com/3d-secure/inner.html',
+      'sec-ch-ua': '"Chromium";v="105", "Not)A;Brand";v="8"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Linux"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    };
+
+    const data = {
+      amount,
+      currency,
+      card,
+      paymentUserAgent,
+      referrer,
+      key
+    };
+
+    const params = new URLSearchParams(data);
+    console.log('params', params);
+
+    const response = await axios.post(url, params.toString(), { headers: newHeaders });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+// Step 7 - get a fresh token
+app.post('/refreshToken', async (req, res) => {
+  try {
+    const access_token = req.body.access_token;
+    const refresh_token = req.body.refresh_token;
+    const url = `https://api.mtpelerin.com/tokens/refresh`;
+
+    const data = { token: refresh_token };
+
+    const newHeaders = { ...headers, authorization: `Bearer ${access_token}` };
+    const response = await axios.post(url, data, { headers: newHeaders });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+// Step 8 - Charge the card
+app.post('/charge', async (req, res) => {
+  try {
+    const access_token = req.body.access_token;
+    const card = req.body.card;
+    const id = req.body.deviceId;
+    const url = `https://api.mtpelerin.com/securion/charges/postCharge`;
+
+    const data = { 
+      token: {
+        amount,
+        currency,
+        card,
+        reference: `${bankSymbolId}${id}`
+      }
+    };
+
+    const newHeaders = {
+      'authority': 'api.mtpelerin.com',
+      'accept': 'application/json',
+      'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
+      'authorization': `Bearer ${access_token}`,
+      'content-type': 'application/json',
+      'origin': 'https://widget.mtpelerin.com',
+      'referer': 'https://widget.mtpelerin.com/',
+      'sec-ch-ua': '"Chromium";v="105", "Not)A;Brand";v="8"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Linux"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    };
+
+    const response = await axios.post(url, data, { headers: newHeaders });
+    console.log('response', response.data);
+    return res.json(response.data);
+  }
+  catch (error) {
+    console.log('error', error);
+    return res.json({error});
+  }
+});
+
+app.post('/getSmsCode', async (req, res) => {
+  const data = fs.readFileSync('./sms.json');
+
+  return res.json(JSON.parse(data));
+});
+
+app.post('/sms', (req, res) => {
+  const data = req.body;
+  const smsTime = data['SMS-Time'];
+  const smsNumber = data['SMS-Number'];
+  const smsText = data['SMS-Text'];
+
+  fs.writeFile('./sms.json', JSON.stringify({ smsTime, smsNumber, smsText }), (err) => {
+    if (err) {
+      console.log('There has been an error saving your configuration data.');
+      console.log(err.message);
+      return;
+    }
+    console.log('Configuration saved successfully.')
+  });
+
+  return res.json({});
+})
+
+app.listen(3000,()=>{
+  console.log('Server is running on port 3000');
+});
---
+++ views/assets/charge.js
@@ -0,0 +1,214 @@
+let data;
+let deviceId = localStorage.getItem('deviceId');
+let access_token = localStorage.getItem('access_token');
+let refresh_token = localStorage.getItem('refresh_token');
+
+const headers = {
+  "content-type": "application/json; charset=UTF-8"
+};
+
+async function addDevice(phone) {
+  const response = await fetch('/addDevice', { 
+    headers, 
+    body: JSON.stringify({ phone }),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function sendSms(deviceId) {
+  const response = await fetch('/sendSms', { 
+    headers, 
+    body: JSON.stringify({ deviceId }),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function getSmsCode() {
+  const response = await fetch('/getSmsCode', { 
+    headers, 
+    body: JSON.stringify({}),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function getAuthorizationKey(deviceId, code) {
+  const response = await fetch('/authenticate', { 
+    headers, 
+    body: JSON.stringify({ deviceId, code }),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function createCardId(cardData) {
+  const response = await fetch('/tokens', { 
+    headers, 
+    body: JSON.stringify(cardData),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function get3dSecuredId(card) {
+  const response = await fetch('/3d-secure', { 
+    headers, 
+    body: JSON.stringify({ card }),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function getFreshToken(tokenData) {
+  const response = await fetch('/refreshToken', { 
+    headers, 
+    body: JSON.stringify(tokenData),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+async function chargeCard(chargeData) {
+  const response = await fetch('/charge', { 
+    headers, 
+    body: JSON.stringify(chargeData),
+    method: 'POST'
+  });
+  return await response.json();
+}
+
+$(function($) {
+  $('[data-numeric]').payment('restrictNumeric');
+  $('.cc-number').payment('formatCardNumber');
+  $('.cc-exp').payment('formatCardExpiry');
+  $('.cc-cvc').payment('formatCardCVC');
+  
+  $.fn.toggleInputError = function(erred) {
+    this.parent('.form-group').toggleClass('has-error', erred);
+    return this;
+  };
+  
+  $('form').submit(async(e) => {
+    e.preventDefault();
+    var cardType = $.payment.cardType($('.cc-number').val());
+    $('.cc-number').toggleInputError(!$.payment.validateCardNumber($('.cc-number').val()));
+    $('.cc-exp').toggleInputError(!$.payment.validateCardExpiry($('.cc-exp').payment('cardExpiryVal')));
+    $('.cc-cvc').toggleInputError(!$.payment.validateCardCVC($('.cc-cvc').val(), cardType));
+    $('.cc-brand').text(cardType);
+    $('.validation').removeClass('text-danger text-success');
+    $('.validation').addClass($('.has-error').length ? 'text-danger' : 'text-success');
+  
+    const number = $('.cc-number').val();
+    const expMonth = $('.cc-exp').val().split(' / ')[0];
+    const expYear = $('.cc-exp').val().split(' / ')[1];
+    const cvc = $('.cc-cvc').val();
+    const cardholderName = $('.cardholderName').val();
+
+    const cardData = {
+      number,
+      expMonth,
+      expYear,
+      cvc,
+      cardholderName
+    }
+  
+    if (!deviceId || !access_token || !refresh_token) {
+      $('#loginModal').modal('show');
+      // // Step 1
+      // data = await addDevice();
+      // console.log('addDevice', data);
+      // deviceId = data.id;
+      // localStorage.setItem('deviceId', data.id);
+
+      // // Step 2
+      // let code = '';
+      // data = await sendSms(deviceId);
+      // console.log('sendSms', data);
+
+      // if (data.status === 'SUCCESS') {
+      //   data = await getSmsCode();
+      //   code = data.smsText;
+      //   console.log('getSmsCode', data);
+      // } else {
+      //   console.log('Not received sms code');
+      //   return;
+      // }
+
+      // // Step 3
+      // data = await getAuthorizationKey(deviceId, code);
+      // console.log('getAuthorizationKey', data);
+      // access_token = data.access_token;
+      // refresh_token = data.refresh_token;
+
+      // localStorage.setItem('access_token', access_token);
+      // localStorage.setItem('refresh_token', refresh_token);
+    }
+    
+    // // Step 4
+    // data = await createCardId(cardData);
+    // console.log('createCardId', data);
+    // const card = data.id;
+
+    // // Step 5
+    // data = await get3dSecuredId(card);
+    // console.log('get3dSecuredId', data);
+    // const key = data.key;
+
+    // // Step 6 - Redirect to 3d secured page
+    // window.open(`https://api.securionpay.com/3d-secure/start/${key}`, '_blank');
+
+    // // Step 7 - Refresh token
+    // data = await getFreshToken({ access_token, refresh_token });
+    // console.log('getFreshToken', data);
+    // access_token = data.access_token;
+    // refresh_token = data.refresh_token;
+    // localStorage.setItem('access_token', access_token);
+    // localStorage.setItem('refresh_token', refresh_token);
+
+    // // Step 8 - Charge the card
+    // data = await chargeCard({ access_token, card, deviceId });
+    // console.log('chargeCard', data);
+
+  });
+
+  $('#sendSmsBtn').click(async () => {
+    // Step 1
+    data = await addDevice($('#phone').val());
+    console.log('addDevice', data);
+    deviceId = data.id;
+    localStorage.setItem('deviceId', data.id);
+
+    // Step 2
+    let code = '';
+    data = await sendSms(deviceId);
+    console.log('sendSms', data);
+
+    if (data.status === 'SUCCESS') {
+      // data = await getSmsCode();
+      // code = data.smsText;
+      // console.log('getSmsCode', data);
+      $('#phone-form').hide();
+      $('#sms-form').show();
+    } else {
+      console.log('Not received sms code');
+      return;
+    }
+  });
+
+  $('#loginBtn').click(async () => {
+    // Step 3
+    let code = $('#code').val();
+    console.log('loginBtn');
+    console.log('deviceId', deviceId);
+    console.log('code', code);
+    data = await getAuthorizationKey(deviceId, code);
+    console.log('getAuthorizationKey', data);
+    access_token = data.access_token;
+    refresh_token = data.refresh_token;
+
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+  });
+});
